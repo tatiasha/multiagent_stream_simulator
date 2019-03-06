53a2f59667c2a7d436b48c577e7e5980b0ca47de
tatiasha <kutuzova.tanya@mail.ru>
2019-03-06 14:36:51 +0300

    refactoring

--- src/model/itmo/escience/streamsimulator/general/OperatorsShareSchedule.scala
+++
@@ -1,8 +0,0 @@
-package itmo.escience.streamsimulator.general
-
-import itmo.escience.streamsimulator.{ClusterCpusShare, OperatorId}
-
-/**
-  * Created by nikolay on 29.03.17.
-  */
-case class OperatorsShareSchedule(shares: Map[OperatorId, ClusterCpusShare]) extends Schedule
--- src/model/itmo/escience/streamsimulator/general/Schedule.scala
+++
@@ -1,8 +0,0 @@
-package itmo.escience.streamsimulator.general
-
-/**
-  * Created by nikolay on 27.03.17.
-  */
-trait Schedule extends Cloneable{
-  override def clone(): AnyRef = super.clone()
-}
